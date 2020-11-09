cfc48be66a1cbc3fc7818f8cb3621556799299b4
Kevin Meagher <11620178+kjmeagher@users.noreply.github.com>
2020-11-09 18:49:19 +0000

    add PrimaryWeightery

---
+++ python/PrimaryWeighter.py
@@ -0,0 +1,34 @@
+import numpy as np
+from . import UprightCylinder,ZenithBias,PowerLaw,GenerationSurface
+from .WeighterBase import make_weighter
+
+def primary_injector_surface_func(
+        primary_type,n_events, cylinder_height,cylinder_radius,min_zenith,
+        max_zenith,min_energy,max_energy,power_law_index,**kwargs):
+    
+    surface = UprightCylinder(cylinder_height,cylinder_radius,
+                              np.cos(max_zenith),np.cos(min_zenith),
+                              ZenithBias.VolumeCorr)
+    assert(power_law_index<0)
+    spectrum = PowerLaw(power_law_index, min_energy, max_energy)        
+    s = GenerationSurface(primary_type, n_events, spectrum, surface)
+    return s
+
+def primary_injector_info_func(table):
+    raise Error("primary injector must have S-Frame")
+
+def primary_injector_event_data(weight_table):
+    return dict(energy       = weight_table.cols.energy[:],
+                type         = weight_table.cols.type[:].astype(np.int32),
+                cos_zenith   = np.cos(weight_table.cols.zenith[:]),
+                weight       = weight_table.cols.weight[:],
+                )
+
+primary_injector_flux_map = dict(E='energy', ptype='type')
+
+PrimaryWeighter = make_weighter("I3PrimaryInjectorInfo",
+                                "I3CorsikaWeight",
+                                primary_injector_surface_func,
+                                primary_injector_info_func,
+                                primary_injector_event_data,
+                                primary_injector_flux_map)
