632d7a8c0b38f27079c01ac01739d87c09a08238
mjlarson <mlarson@icecube.wisc.edu>
2024-08-29 14:09:13 -0500

    Use the right dist in the dataset tests.

--- tests/test_genie_datasets.py
+++ tests/test_genie_datasets.py
@@ -61,8 +61,10 @@ def test_dataset(fname):
 
         assert w.get_weight_column("wght") == approx(genie_weight)
 
-        power_law = next(iter(w.surface.spectra.values()))[0].dists[2]
+        power_law = next(iter(w.surface.spectra.values()))[0].dists[-1]
         energy_term = 1 / power_law.pdf(w.get_weight_column("energy"))
+        
+        print(energy_term / energy_factor)
         assert energy_term == approx(energy_factor)
 
         one_weight = w.get_weight_column("wght") * energy_term * solid_angle * injection_area * global_probability_scale
