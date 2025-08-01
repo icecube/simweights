icecube/simweights

commit 1e96449ae56d526eaa899b1ae4d51b56b1fc5e3e

Make PDFs more explicit
One of the criticistms of simweights is that it is hard to find the
weighting functions. This was because the weighting was a list of
objects and the weights were calculating by taking the product of them.
This PR refactors it so that each type of simulation has a class that
is derived from GenerationSurface that overrides get_epdf(). This is a
clear function that is 1/weight. However since it makes use of Spatial
and PowerLaw it is not as explicit as it could be but DRY. As a side
effecth Spatial disturcitions and PowerLaw no longer have column names
as a member variable which i thought was just weird.

pyproject.toml
@@ -70,7 +70,6 @@ max-line-length = 128
 
 [tool.mypy]
 enable_error_code = ["ignore-without-code", "redundant-expr", "truthy-bool"]
-plugins = "numpy.typing.mypy_plugin"
 strict = true
 warn_unreachable = true
 
@@ -84,7 +83,7 @@ disable = "C0114,R0902,R0913,R0917,R0914,R0911"
 addopts = ["-ra", "--strict-config", "--strict-markers", "--cov=simweights", "-W ignore"]
 filterwarnings = ["error"]
 log_cli_level = "INFO"
-minversion = 7.0
+minversion = "7.0"
 testpaths = ["tests"]
 xfail_strict = true
 
@@ -93,7 +92,7 @@ line-length = 128
 namespace-packages = ["examples", "contrib", "docs"]
 
 [tool.ruff.lint]
-fixable = ["I"]
+fixable = ["I", "Q"]
 ignore = [
   "ANN401",  # any-type
   "S101",  # assert-used

src/simweights/__init__.py
@@ -24,6 +24,7 @@ __all__ = [
     "GaisserH4a_IT",
     "GaisserHillas",
     "GenerationSurface",
+    "GenieSurface",
     "GenieWeighter",
     "GlobalFitGST",
     "GlobalFitGST_IT",
@@ -34,15 +35,16 @@ __all__ = [
     "Hoerandel5",
     "Hoerandel_IT",
     "Honda2004",
+    "IceTopSurface",
     "IceTopWeighter",
     "NaturalRateCylinder",
+    "NuGenSurface",
     "NuGenWeighter",
     "PDGCode",
     "PowerLaw",
     "UniformSolidAngleCylinder",
     "Weighter",
     "corsika_to_pdg",
-    "generation_surface",
 ]
 
 from ._corsika_weighter import CorsikaWeighter
@@ -63,10 +65,10 @@ from ._fluxes import (
     Hoerandel_IT,
     Honda2004,
 )
-from ._generation_surface import GenerationSurface, generation_surface
-from ._genie_weighter import GenieWeighter
-from ._icetop_weighter import IceTopWeighter
-from ._nugen_weighter import NuGenWeighter
+from ._generation_surface import GenerationSurface
+from ._genie_weighter import GenieSurface, GenieWeighter
+from ._icetop_weighter import IceTopSurface, IceTopWeighter
+from ._nugen_weighter import NuGenSurface, NuGenWeighter
 from ._pdgcode import PDGCode
 from ._powerlaw import PowerLaw
 from ._spatial import CircleInjector, NaturalRateCylinder, UniformSolidAngleCylinder

tests/test_genie_icetray_weighter.py
@@ -27,6 +27,7 @@ mcwd_keys = [
     "InjectionSurfaceR",
     "PrimaryNeutrinoEnergy",
     "GeneratorVolume",
+    "GlobalProbabilityScale",
 ]
 
 grd_keys = ["neu", "pxv", "pyv", "pzv", "Ev", "wght", "_glbprbscale"]
@@ -44,6 +45,7 @@ def make_new_table(pdgid, nevents, spatial, spectrum, coszen):
     weight["InjectionSurfaceR"] = spatial.radius
     weight["GeneratorVolume"] = 1.0
     weight["PrimaryNeutrinoEnergy"] = spectrum.ppf(np.linspace(0, 1, nevents, endpoint=False))
+    weight["GlobalProbabilityScale"] = 1.0
 
     dtype = [(k, float) for k in grd_keys]
     resultdict = np.zeros(nevents, dtype=dtype)

tests/test_genie_weighter.py
@@ -31,6 +31,23 @@ info_dtype = [
 result_dtype = [("neu", np.int32), ("pzv", np.float64), ("Ev", np.float64), ("wght", np.float64)]
 
 
+def test_genie_repr():
+    c1 = simweights.CircleInjector(300, 0, 1)
+    p1 = simweights.PowerLaw(0, 1e3, 1e4)
+    g = simweights.GenieSurface(-14, 1000, 0.3333, c1, p1)
+    assert (
+        eval(
+            repr(g),
+            locals={
+                "GenieSurface": simweights.GenieSurface,
+                "CircleInjector": simweights.CircleInjector,
+                "PowerLaw": simweights.PowerLaw,
+            },
+        )
+        == g
+    )
+
+
 @pytest.mark.parametrize("event_weight", (1e-6, 1e-3, 1))
 @pytest.mark.parametrize("nfiles", (1, 5, 50))
 @pytest.mark.parametrize("include_volscale", (True, False))

tests/test_nugen_weighter.py
@@ -13,7 +13,8 @@ import pytest
 from numpy.testing import assert_allclose
 from pytest import approx
 
-from simweights import CircleInjector, NuGenWeighter, PowerLaw, UniformSolidAngleCylinder
+from simweights import CircleInjector, NaturalRateCylinder, NuGenSurface, NuGenWeighter, PowerLaw, UniformSolidAngleCylinder
+from tests.test_generation_surface import CompositeSurface
 
 with contextlib.suppress(ImportError):
     from icecube import dataclasses, icetray
@@ -46,6 +47,36 @@ def make_new_table(pdgid, nevents, spatial, spectrum):
     )
     weight["PrimaryNeutrinoEnergy"] = spectrum.ppf(np.linspace(0, 1, nevents))
     return weight
+
+
+def test_two_nugen_surfaces():
+    p1 = PowerLaw(-1, 10, 100)
+    p2 = PowerLaw(-2, 50, 500)
+    c1 = NaturalRateCylinder(3, 8, -1, 1)
+
+    N1 = 10000
+    N2 = 20000
+    s0 = NuGenSurface(2212, N1, p1, c1)
+    s2 = NuGenSurface(2212, N2, p2, c1)
+
+    cz1 = np.linspace(c1.cos_zen_min, c1.cos_zen_max, N1)
+    cz2 = np.linspace(c1.cos_zen_min, c1.cos_zen_max, N2)
+    q1 = np.linspace(1 / 2 / N1, 1 - 1 / 2 / N1, N1)
+    E1 = 10 * np.exp(q1 * np.log(100 / 10))
+    q2 = np.linspace(1 / 2 / N2, 1 - 1 / 2 / N2, N2)
+    E2 = (q2 * (500**-1 - 50**-1) + 50**-1) ** -1
+
+    w1 = 1 / s0.get_epdf({"pdgid": 2212, "energy": E1, "cos_zen": cz1, "TotalWeight": 1})
+    assert w1.sum() / (p1.b - p1.a) / c1.etendue == approx(1)
+    w2 = 1 / s2.get_epdf({"pdgid": 2212, "energy": E2, "cos_zen": cz2, "TotalWeight": 1})
+    assert w2.sum() / (p2.b - p2.a) / c1.etendue == approx(1)
+
+    surf = CompositeSurface(s0, s2)
+    E = np.r_[E1, E2]
+    czc = np.r_[cz1, cz2]
+    wc = 1 / surf.get_epdf({"pdgid": 2212, "energy": E, "cos_zen": czc, "TotalWeight": 1})
+
+    assert wc.sum() / (p2.b - p1.a) / c1.etendue == approx(1, 1e-5)
 
 
 @pytest.mark.parametrize("weight", (0.1, 1, 10))

.pre-commit-config.yaml
@@ -28,7 +28,7 @@ repos:
       - id: blacken-docs
         args: [-l 100]
   - repo: https://github.com/pre-commit/mirrors-mypy
-    rev: v1.16.1
+    rev: v1.17.1
     hooks:
       - id: mypy
         files: simweights
@@ -42,7 +42,7 @@ repos:
         exclude: ^contrib/
         additional_dependencies: [numpy, pandas]
   - repo: https://github.com/astral-sh/ruff-pre-commit
-    rev: v0.12.2
+    rev: v0.12.7
     hooks:
       - id: ruff
         args: [--fix, --show-fixes]

src/simweights/_corsika_weighter.py
@@ -6,24 +6,40 @@ from __future__ import annotations
 
 import numbers
 import warnings
-from typing import Any
+from typing import TYPE_CHECKING, Any, Mapping
 
 import numpy as np
 
-from ._generation_surface import GenerationSurface, generation_surface
+from ._generation_surface import CompositeSurface, GenerationSurface
 from ._powerlaw import PowerLaw
 from ._spatial import NaturalRateCylinder
-from ._utils import Column, constcol, get_column, get_table, has_column, has_table
+from ._utils import constcol, get_column, get_table, has_column, has_table
 from ._weighter import Weighter
 
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
 
-def sframe_corsika_surface(table: Any) -> GenerationSurface:
+
+class CorsikaSurface(GenerationSurface):
+    """Represents a surface on which CORSIKA simulation was generated on."""
+
+    def get_epdf(self: CorsikaSurface, weight_cols: Mapping[str, NDArray[np.float64]]) -> NDArray[np.float64]:
+        """Get the extended pdf of a sample of CORSIKA."""
+        return (
+            self.nevents
+            / weight_cols["event_weight"]
+            * self.power_law.pdf(weight_cols["energy"])
+            * self.spatial.pdf(weight_cols["cos_zen"])
+        )
+
+
+def sframe_corsika_surface(table: Any) -> CompositeSurface:
     """Inspect the rows of a CORSIKA S-Frame table object to generate a surface object.
 
     This function works on files generated with either triggered CORSIKA or corsika-reader because
     `I3PrimaryInjectorInfo` and `I3CorsikaInfo` use exactly the same names for quantities.
     """
-    surfaces = []
+    surfaces = CompositeSurface()
     cylinder_height = get_column(table, "cylinder_height")
     cylinder_radius = get_column(table, "cylinder_radius")
     max_zenith = get_column(table, "max_zenith")
@@ -39,32 +55,26 @@ def sframe_corsika_surface(table: Any) -> GenerationSurface:
             cylinder_radius[i],
             np.cos(max_zenith[i]),
             np.cos(min_zenith[i]),
-            "cos_zen",
         )
         spectrum = PowerLaw(
             power_law_index[i],
             min_energy[i],
             max_energy[i],
-            "energy",
         )
         oversampling_val = get_column(table, "oversampling")[i] if has_column(table, "oversampling") else 1
         pdgid = int(get_column(table, "primary_type")[i])
-        surfaces.append(
-            n_events[i] * oversampling_val * generation_surface(pdgid, Column("event_weight"), spectrum, spatial),
-        )
-    retval = sum(surfaces)
-    assert isinstance(retval, GenerationSurface)
-    return retval
+        surfaces.insert(CorsikaSurface(pdgid, n_events[i] * oversampling_val, spectrum, spatial))
+    return surfaces
 
 
-def weight_map_corsika_surface(table: Any) -> GenerationSurface:
+def weight_map_corsika_surface(table: Any) -> CompositeSurface:
     """Inspect the `CorsikaWeightMap` table object of a corsika file to generate a surface object."""
     pdgids = sorted(np.unique(get_column(table, "ParticleType").astype(int)))
 
     if len(pdgids) == 0:
         msg = "`CorsikaWeightMap` is empty. SimWeights cannot process this file"
         raise RuntimeError(msg)
-    surface: int | GenerationSurface = 0
+    surface = CompositeSurface()
     for pdgid in pdgids:
         mask = pdgid == get_column(table, "ParticleType")
 
@@ -73,7 +83,6 @@ def weight_map_corsika_surface(table: Any) -> GenerationSurface:
             constcol(table, "CylinderRadius", mask),
             np.cos(constcol(table, "ThetaMax", mask)),
             np.cos(constcol(table, "ThetaMin", mask)),
-            "cos_zen",
         )
 
         primary_spectral_index = round(constcol(table, "PrimarySpectralIndex", mask), 6)
@@ -83,11 +92,9 @@ def weight_map_corsika_surface(table: Any) -> GenerationSurface:
             primary_spectral_index,
             constcol(table, "EnergyPrimaryMin", mask),
             constcol(table, "EnergyPrimaryMax", mask),
-            "energy",
         )
         nevents = constcol(table, "OverSampling", mask) * constcol(table, "NEvents", mask)
-        surface += nevents * generation_surface(pdgid, spectrum, spatial)
-    assert isinstance(surface, GenerationSurface)
+        surface.insert(CorsikaSurface(pdgid, nevents, spectrum, spatial))
     return surface
 
 
@@ -145,7 +152,8 @@ def CorsikaWeighter(file_obj: Any, nfiles: float | None = None) -> Weighter:  # 
             )
 
         table = get_table(file_obj, "CorsikaWeightMap")
-        surface = nfiles * weight_map_corsika_surface(table)
+        surface = weight_map_corsika_surface(table)
+        surface.scale(nfiles)
         triggered = False
 
     weighter = Weighter([file_obj], surface)

commit 5ef4eae28137d3940b9adfea50a752e842d8fd97

fix issues introduced by 1e96449
That commit was supposed to be a PR but i accedently commited it
directly to main

pyproject.toml
@@ -132,7 +132,7 @@ convention = "google"
 [tool.tox]
 legacy_tox_ini = """
 [tox]
-envlist = py3{9,10,11,12,13}
+envlist = py3{10,11,12,13}
 isolated_build = True
 
 [testenv]

src/simweights/__init__.py
@@ -17,6 +17,7 @@ __version__ = "0.1.3"
 __all__ = [
     "TIG1996",
     "CircleInjector",
+    "CompositeSurface",
     "CorsikaWeighter",
     "FixedFractionFlux",
     "GaisserH3a",
@@ -65,7 +66,7 @@ from ._fluxes import (
     Hoerandel_IT,
     Honda2004,
 )
-from ._generation_surface import GenerationSurface
+from ._generation_surface import CompositeSurface, GenerationSurface
 from ._genie_weighter import GenieSurface, GenieWeighter
 from ._icetop_weighter import IceTopSurface, IceTopWeighter
 from ._nugen_weighter import NuGenSurface, NuGenWeighter

tests/test_generation_surface.py
@@ -160,12 +160,13 @@ class Testgeneration_surface(unittest.TestCase):
         self.assertEqual(self.s0.nevents, 10000)
 
     def test_repr(self):
-        ll = {"Gamma": PDGCode.Gamma, "PPlus": PDGCode.PPlus}
-        self.assertEqual(eval(repr(self.s0), locals=ll), self.s0)
-        self.assertEqual(eval(repr(self.s1), locals=ll), self.s1)
-        self.assertEqual(eval(repr(self.s2), locals=ll), self.s2)
-        self.assertEqual(eval(repr(self.s3), locals=ll), self.s3)
-        self.assertEqual(eval(repr(self.s4), locals=ll), self.s4)
+        Gamma = PDGCode.Gamma  # noqa: F841
+        PPlus = PDGCode.PPlus  # noqa: F841
+        self.assertEqual(eval(repr(self.s0)), self.s0)
+        self.assertEqual(eval(repr(self.s1)), self.s1)
+        self.assertEqual(eval(repr(self.s2)), self.s2)
+        self.assertEqual(eval(repr(self.s3)), self.s3)
+        self.assertEqual(eval(repr(self.s4)), self.s4)
 
     def test_addition_gsc(self):
         s0 = CompositeSurface(self.gsc1, self.s0)
@@ -244,11 +245,12 @@ class Testgeneration_surface(unittest.TestCase):
         self.assertEqual(self.gsc4, CompositeSurface(self.s4, self.s0))
 
     def test_repr_gsc(self):
-        ll = {"PPlus": PDGCode.PPlus, "Gamma": PDGCode.Gamma}
-        self.assertEqual(self.gsc1, eval(repr(self.gsc1), locals=ll))
-        self.assertEqual(self.gsc2, eval(repr(self.gsc2), locals=ll))
-        self.assertEqual(self.gsc3, eval(repr(self.gsc3), locals=ll))
-        self.assertEqual(self.gsc4, eval(repr(self.gsc4), locals=ll))
+        PPlus = PDGCode.PPlus  # noqa: F841
+        Gamma = PDGCode.Gamma  # noqa: F841
+        self.assertEqual(self.gsc1, eval(repr(self.gsc1)))
+        self.assertEqual(self.gsc2, eval(repr(self.gsc2)))
+        self.assertEqual(self.gsc3, eval(repr(self.gsc3)))
+        self.assertEqual(self.gsc4, eval(repr(self.gsc4)))
 
         s = str(CompositeSurface(self.gsc2, self.gsc3, self.gsc4)).split("\n")
         self.assertEqual(s[0], "< CompositeSurface")

tests/test_genie_icetray_weighter.py
@@ -117,6 +117,7 @@ def test_genie_icetray_i3files(nfiles, flux, nevents):
     weight["InjectionSurfaceR"] = c1.radius
     weight["GeneratorVolume"] = 1.0
     weight["PrimaryNeutrinoEnergy"] = energy
+    weight["GlobalProbabilityScale"] = 1
 
     resultdict = simclasses.I3GENIEResultDict()
     resultdict.neu = pdgid

tests/test_genie_weighter.py
@@ -35,17 +35,10 @@ def test_genie_repr():
     c1 = simweights.CircleInjector(300, 0, 1)
     p1 = simweights.PowerLaw(0, 1e3, 1e4)
     g = simweights.GenieSurface(-14, 1000, 0.3333, c1, p1)
-    assert (
-        eval(
-            repr(g),
-            locals={
-                "GenieSurface": simweights.GenieSurface,
-                "CircleInjector": simweights.CircleInjector,
-                "PowerLaw": simweights.PowerLaw,
-            },
-        )
-        == g
-    )
+    GenieSurface = simweights.GenieSurface  # noqa: F841
+    CircleInjector = simweights.CircleInjector  # noqa: F841
+    PowerLaw = simweights.PowerLaw  # noqa: F841
+    assert eval(repr(g)) == g
 
 
 @pytest.mark.parametrize("event_weight", (1e-6, 1e-3, 1))

tests/test_nugen_weighter.py
@@ -13,8 +13,15 @@ import pytest
 from numpy.testing import assert_allclose
 from pytest import approx
 
-from simweights import CircleInjector, NaturalRateCylinder, NuGenSurface, NuGenWeighter, PowerLaw, UniformSolidAngleCylinder
-from tests.test_generation_surface import CompositeSurface
+from simweights import (
+    CircleInjector,
+    CompositeSurface,
+    NaturalRateCylinder,
+    NuGenSurface,
+    NuGenWeighter,
+    PowerLaw,
+    UniformSolidAngleCylinder,
+)
 
 with contextlib.suppress(ImportError):
     from icecube import dataclasses, icetray